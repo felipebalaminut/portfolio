26275986a3abd9cd1ff8f44674e121d82ec990a9
Felipe Fonseca <felipebalaminutdev@gmail.com>
2025-09-19 10:53:35 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="relative min-h-screen bg-[url('src/assets/bg-header-mobile.png')] md:bg-[url('src/assets/bg-header-tablet.png')] xl:bg-[url('src/assets/bg-header.png')] bg-top bg-no-repeat">
+      
+    </header>
+  );
+}
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header/index";
+
+export function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
